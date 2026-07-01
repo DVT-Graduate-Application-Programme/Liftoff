2df3412cca7a7762226cab18d599fab3e9ae96f8
Troy Clark <TClark@dvtsoftware.com>
2026-07-01 14:23:05 +0200

    feat: Candidate-list page, style tweaks and set default tab

--- frontend/src/app/landing/page.tsx
+++ frontend/src/app/landing/page.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PendingCandidates from "@/components/pendingCandidates/PendingCandidates";
 import AllCandidates from "@/components/allCandidates/AllCandidates";
+import { Input } from "@/components/ui/input";
 
 const page = () => {
   return (
-    <div className="w-full">
-      <input className="w-1/2 border border-accent width-50" />
-      <Tabs>
+    <div className="w-full flex flex-col items-center gap-4 p-4">
+      <Input className="w-1/2 border border-accent" />
+      <Tabs defaultValue="pending">
         <TabsList>
           <TabsTrigger value="pending">Pending</TabsTrigger>
           <TabsTrigger value="all">All Candidates</TabsTrigger>
