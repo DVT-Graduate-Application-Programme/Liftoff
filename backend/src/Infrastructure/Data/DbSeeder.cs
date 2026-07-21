153c7114501e42dea235fdcfeafff73079adc793
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-07-21 11:29:34 +0200

    feat: changed the naming conflict issue

--- backend/src/Infrastructure/Data/DbSeeder.cs
+++ backend/src/Infrastructure/Data/DbSeeder.cs
@@ -57,8 +57,8 @@ public static class DbSeeder
         await db.Database.ExecuteSqlRawAsync("""
             INSERT INTO public."Recruiters" ("Id", "IdentityId", "FirstName", "LastName", "Email", "IsActive", "CreatedAt", "UpdatedAt")
             VALUES
-                ('c1000000-0000-0000-0000-000000000001', 'Phindi.Gamede@dvt.co.za', 'Phindile',  'Gamede', 'Phindi.Gamede@dvt.co.za',  TRUE, NOW(), NOW()),
-                ('c2000000-0000-0000-0000-000000000002', 'Rose.Allen-Richards@dvt.co.za',     'Rose',   'Allen-Richards',  'Rose.Allen-Richards@dvt.co.za',    TRUE, NOW(), NOW())
+                ('c1000000-0000-0000-0000-000000000001', 'phindi@dvtsoftware.com', 'Phindile',  'Gamede', 'phindi@dvtsoftware.com',  TRUE, NOW(), NOW()),
+                ('c2000000-0000-0000-0000-000000000002', 'rose@dvtsoftware.com',     'Rose',   'Allen-Richards',  'rose@dvtsoftware.com',    TRUE, NOW(), NOW())
             ON CONFLICT ("IdentityId") DO NOTHING;
             """);
 
