ee1bf9df8908cd733689188bc0f044214b5a8bcf
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-07-31 15:13:57 +0200

    fix: Added hiring agent evaluation dto and added sum validation in order to make sure the dto adheres to the api contract.

--- backend/src/Application/DTOs/ApplicationDetails.cs
+++ backend/src/Application/DTOs/ApplicationDetails.cs
@@ -1,3 +1,4 @@
+using Application.DTOs;
 using Domain.Entities;
 
 public class ApplicationDetails
@@ -7,7 +8,7 @@ public class ApplicationDetails
     public string? Tier { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset UpdatedAt { get; set; }
-    public ICollection<HiringAgentEvaluation> HiringAgentEvaluations { get; set; } = new List<HiringAgentEvaluation>();
+    public ICollection<HiringAgentEvaluationDto> HiringAgentEvaluations { get; set; } = new List<HiringAgentEvaluationDto>();
     public ICollection<RecruiterAction> RecruiterActions { get; set; } = new List<RecruiterAction>();
     public ICollection<AuditLog> AuditLogs { get; set; } = new List<AuditLog>();
 }
